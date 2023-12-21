c57d4578ed5e0d34b29b4d34134b7d3a615b6578
n-sweep <34486798+n-sweep@users.noreply.github.com>
2023-12-20 22:27:15 -0500

    auto-open web browser

--- run_flask.py
+++ run_flask.py
@@ -2,6 +2,7 @@ import os
 import time
 import socket
 import logging
+import webbrowser
 
 from flask_socketio import SocketIO
 from app import app
@@ -11,19 +12,23 @@ socketio = SocketIO(app, cors_allowed_origins="*", transport='websocket')
 
 @socketio.on('connect')
 def handle_connect():
+    """ once connected, start file watcher """
     handler = Handler(app, socketio)
     socketio.start_background_task(target=handler.watch_for_modification)
 
 
 class Handler:
+    """ file handler watches for and emits updates to plot file """
     def __init__(self, app, socketio):
         self.mt = 0
         self.app = app
         self.socketio = socketio
 
         self.ensure_directory()
+        logging.info('handler started')
 
     def watch_for_modification(self):
+        """ look for change in modified time of `file` every `i` ms """
         file = self.app.config['file']
         i = self.app.config['interval']
 
@@ -39,6 +44,7 @@ class Handler:
             self.socketio.sleep(i)
 
     def ensure_directory(self):
+        """ ensure the tmp directory exists before attempting to save a file """
         file = self.app.config['file']
         dir = os.path.dirname(file)
         if not os.path.exists(dir):
@@ -48,16 +54,19 @@ class Handler:
 def main():
     host_name = socket.getfqdn()
     ip = socket.gethostbyname(host_name)
+    port = 5619
     logging.basicConfig(filename='pvserv.log', level=logging.INFO)
 
     config = {
         'file': '/tmp/vis/plot.json',
-        'interval': 0.1
+        'interval': 0.1,
+        'url': f'http://{ip}:{port}'
     }
 
     app.config.update(config)
 
-    socketio.run(app, host=ip, port=5619, debug=True)
+    webbrowser.open_new(app.config['url'])
+    socketio.run(app, host=ip, port=port, debug=True)
 
 
 if __name__ == "__main__":
