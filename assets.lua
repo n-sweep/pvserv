5f2102f4de2c1f1c50ea11a919072973b483a6e2
n-sweep <34486798+n-sweep@users.noreply.github.com>
2024-02-26 09:47:33 -0500

    restructuring

--- assets.lua
+++ assets.lua
@@ -7,7 +7,7 @@ function M.start_server()
         print('Server is already running')
     else
         local script_path = debug.getinfo(1).short_src
-        local server_path = script_path:gsub("/[^/]+$", "/server/run.py")
+        local server_path = script_path:gsub("/lua/telemux/[^/]+$", "/server/run.py")
         job_id = vim.fn.jobstart(
             'python ' .. server_path,
             { on_exit = function() print('Server exited'); job_id = nil end }
