5de9515bd3644ab0343546bb77d5ece070a0a7e2
n-sweep <34486798+n-sweep@users.noreply.github.com>
2026-02-28 07:51:59 -0500

    remove dockerfile, fix db persistence

--- flake.nix
+++ flake.nix
@@ -112,9 +112,6 @@
               "${venv}/bin/python ${./src/manage.py} migrate && ${venv}/bin/python ${./src/manage.py} runserver 0.0.0.0:8080"
             ];
           };
-          # extraCommands = ''
-          #   mkdir -p .data
-          # '';
         };
       });
 
